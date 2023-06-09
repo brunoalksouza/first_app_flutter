8c9dc5d8613c9868d854c4a85de54124bb6d9f29
Bruno Souza <brunoalksouza@gmail.com>
2023-06-09 15:28:49 -0300

    mod 4 finished

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,7 +1,10 @@
+import 'package:first_app_flutter/data/task_inherited.dart';
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatefulWidget {
-  const FormScreen({super.key});
+  const FormScreen({Key? key, required this.taskContext}) : super(key: key);
+
+  final BuildContext taskContext;
 
   @override
   State<FormScreen> createState() => _FormScreenState();
@@ -133,12 +136,14 @@ class _FormScreenState extends State<FormScreen> {
                     ElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
-                          print(nameController.text);
-                          print(int.parse(difficultyController.text));
-                          print(imageController.text);
+                          TaskInherited.of(widget.taskContext).newTask(
+                              nameController.text,
+                              imageController.text,
+                              int.parse(difficultyController.text));
+
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
-                              content: Text('salvando nova tarefa...'),
+                              content: Text('Criando uma nova tarefa...'),
                             ),
                           );
                           Navigator.pop(context);
